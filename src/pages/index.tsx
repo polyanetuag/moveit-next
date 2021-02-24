9af8913f698924600f0d25f1422b22255d90664e
Polyane Tuag <pollyportugues@hotmail.com>
2021-02-24 18:43:06 -0300

    Componente CompletedChallenges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
@@ -11,6 +12,7 @@ export default function Home() {
     <section>
       <div>
           <Profile />
+          <CompletedChallenges />
       </div>
       <div>
 
