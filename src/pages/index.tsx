33eea7442ce44881ffb8225bab6d219151ae4246
Polyane Tuag <pollyportugues@hotmail.com>
2021-02-25 19:17:27 -0300

    Correção nome_arquivo ChallengeBox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import { ChallengeBox } from "../components/Challengebox";
+import { ChallengeBox } from "../components/ChallengeBox";
 import styles from '../styles/pages/Home.module.css';
 
 import Head from 'next/head';
