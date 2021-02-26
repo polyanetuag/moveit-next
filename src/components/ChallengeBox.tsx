7f4b6bcab8c994ec8c0d17d9abed353c2103e698
Polyane Tuag <pollyportugues@hotmail.com>
2021-02-26 19:46:51 -0300

    Anexando as ações aos botões

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,21 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
-  const { activeChallenge, resetChallenge, completeChallenge }= useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext)
+
+  function handleChallengeSucceeded() {
+    completeChallenge();
+    resetCountdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
+  }
   
   return (
     <div className={styles.challengeBoxContainer}>
@@ -21,7 +33,7 @@ export function ChallengeBox() {
             <button 
             type="button"
             className={styles.challengeFailedButton}
-            onClick={resetChallenge}
+            onClick={handleChallengeFailed}
             >
               Falhei
             </button>
@@ -29,7 +41,7 @@ export function ChallengeBox() {
             <button 
             type="button"
             className={styles.challengeSucceededButton}
-            onClick={completeChallenge}
+            onClick={handleChallengeSucceeded}
             >
               Completei
             </button>
