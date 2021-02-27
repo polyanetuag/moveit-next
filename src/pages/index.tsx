b35b8f54a72f3456a0f2cf5b166dced0fabb9eec
Polyane Tuag <pollyportugues@hotmail.com>
2021-02-27 19:37:58 -0300

    Fazendo o storage da aplicação via cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,24 @@ import { ChallengeBox } from "../components/ChallengeBox";
 import styles from '../styles/pages/Home.module.css';
 
 import Head from 'next/head';
+import {GetServerSideProps} from 'next';
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
+    <ChallengesProvider
+    level={props.level}
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}
+    >
+
     <div className={styles.container}>
       <Head>
         <title>Início | move.it</title>
@@ -30,7 +44,19 @@ export default function Home() {
         </div>
       </section>
     </CountdownProvider>
-
   </div>
+  </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),                             //transformando string para number
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
